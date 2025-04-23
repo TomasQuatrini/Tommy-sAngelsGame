4163bb6aac70239925afb02bb01822ffbfca7e3d
TomasQuatrini <tomas_quatrini11@hotmail.com>
2025-04-22 22:23:27 -0300

    create script base

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Item.cs
+// Base class for all items in the game
+// Provides a basic structure for items with a name, description, and use method
+
+using UnityEngine;
+
+public abstract class Item : ScriptableObject
+{
+    // Item name
+    public string name;
+    
+    // Item description
+    public string description;
+
+    // Method to use the item
+    public abstract void Use();
+}
